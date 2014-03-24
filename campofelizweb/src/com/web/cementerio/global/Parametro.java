742a5e990635cd911cad64c0e31978df8c108287
metalkarlos <Metalkarlos0923>
2014-03-24 00:16:53 -0500

    try catch a getrealpath

--- campofelizweb/src/com/web/cementerio/global/Parametro.java
+++ campofelizweb/src/com/web/cementerio/global/Parametro.java
@@ -14,7 +14,17 @@ public class Parametro {
 	public static final String BLANK_IMAGE_PATH = FILE_SEPARATOR+"resources"+FILE_SEPARATOR+"images"+FILE_SEPARATOR+"miscellaneous"+FILE_SEPARATOR+"blank.jpg";
 	public static final String IMAGE_FILE_NAME_PATERN = "##-###-###.jpg";
 	public static final long DAY_IN_MILLISECONDS = (24*60*60*1000);
-	public static final String WAR_PATH = new FacesUtil().getRealPath("");
+	public static final String WAR_PATH;
+	static{
+		String war_path = null;
+		try{
+			war_path = new FacesUtil().getRealPath("");
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		WAR_PATH = war_path;
+	}
 	public static final String DEPLOYMENTS_PATH = WAR_PATH.substring(0, WAR_PATH.lastIndexOf("\\"));
 	public static final String PARAMETROS_PROPERTIES_PATH = WAR_PATH+FILE_SEPARATOR+"resources"+FILE_SEPARATOR+"parametros.properties";
 	public static final String RUTA_REPORTES = WAR_PATH+FILE_SEPARATOR+"reportes"+FILE_SEPARATOR;
