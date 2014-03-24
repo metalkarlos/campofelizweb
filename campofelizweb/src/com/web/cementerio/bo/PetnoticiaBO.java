3344ff753fd8bc93aa74f1087927c9c897218ea3
metalkarlos <Metalkarlos0923>
2014-03-23 20:37:19 -0500

    capas de la noticia

---
+++ campofelizweb/src/com/web/cementerio/bo/PetnoticiaBO.java
@@ -0,0 +1,90 @@
+package com.web.cementerio.bo;
+
+import java.util.Date;
+import java.util.List;
+
+import org.hibernate.Session;
+
+import com.web.cementerio.bean.UsuarioBean;
+import com.web.cementerio.dao.PetnoticiaDAO;
+import com.web.cementerio.pojo.annotations.Petnoticia;
+import com.web.util.FacesUtil;
+import com.web.util.HibernateUtil;
+
+public class PetnoticiaBO {
+
+	PetnoticiaDAO petnoticiaDAO;
+	
+	public PetnoticiaBO() {
+		petnoticiaDAO = new PetnoticiaDAO();
+	}
+	
+	public Petnoticia getPetnoticiaById(int idnoticia) throws Exception {
+		Petnoticia petnoticia = null;
+		Session session = null;
+		
+		try{
+            session = HibernateUtil.getSessionFactory().openSession();
+            petnoticia = petnoticiaDAO.getPetnoticiaById(session, idnoticia);
+        }
+        catch(Exception ex){
+            throw new Exception(ex);
+        }
+        finally{
+            session.close();
+        }
+		
+		return petnoticia;
+	}
+	
+	public List<Petnoticia> lisPetnoticiaByPage(int pageSize, int pageNumber, int args[], String titulo, String descripcion) throws Exception {
+		List<Petnoticia> listPetnoticia = null;
+		Session session = null;
+		
+		try{
+            session = HibernateUtil.getSessionFactory().openSession();
+            listPetnoticia = petnoticiaDAO.lisPetnoticiaByPage(session, pageSize, pageNumber, args, titulo, descripcion);
+        }
+        catch(Exception ex){
+            throw new Exception(ex);
+        }
+        finally{
+            session.close();
+        }
+		
+		return listPetnoticia;
+	}
+	
+	public boolean grabar(Petnoticia petnoticia) throws Exception {
+		boolean ok = false;
+		Session session = null;
+		
+		try{
+			session = HibernateUtil.getSessionFactory().openSession();
+			session.beginTransaction();
+			
+			int maxid = petnoticiaDAO.maxIdnoticia(session)+1;
+			Date fecharegistro = new Date();
+			UsuarioBean usuarioBean = (UsuarioBean)new FacesUtil().getSessionBean("usuarioBean");
+			
+			petnoticia.setIdnoticia(maxid);
+			petnoticia.setFecharegistro(fecharegistro);
+			petnoticia.setIplog(usuarioBean.getIp());
+			petnoticia.setSetestado(usuarioBean.getSetUsuario().getSetestado());
+			petnoticia.setSetusuario(usuarioBean.getSetUsuario());
+	
+			petnoticiaDAO.savePetnoticia(session, petnoticia);
+			
+			session.getTransaction().commit();
+			
+			ok = true;
+		}catch(Exception e){
+			session.getTransaction().rollback();
+			throw new Exception(e); 
+		}finally{
+			session.close();
+		}
+		
+		return ok;
+	}
+}
---
+++ campofelizweb/src/com/web/cementerio/dao/PetnoticiaDAO.java
@@ -0,0 +1,97 @@
+package com.web.cementerio.dao;
+
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Projections;
+import org.hibernate.criterion.Restrictions;
+
+import com.web.cementerio.pojo.annotations.Petnoticia;
+
+public class PetnoticiaDAO {
+
+	public int maxIdnoticia(Session session) throws Exception {
+		int max=0;
+		
+		Object object = session.createQuery("select max(idnoticia) as max from Petnoticia ").uniqueResult();
+		max = (object==null?0:Integer.parseInt(object.toString()));
+		
+		return max;
+	}
+	
+	public Petnoticia getPetnoticiaById(Session session, int idnoticia) throws Exception {
+		Petnoticia petnoticia = null;
+		
+		String hql = " from Petnoticia ";
+		hql += " where idnoticia = :idnoticia ";
+
+		Query query = session.createQuery(hql)
+				.setInteger("idnoticia", idnoticia);
+		
+		petnoticia = (Petnoticia) query.uniqueResult();
+		
+		return petnoticia;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Petnoticia> lisPetnoticiaByPage(Session session, int pageSize, int pageNumber, int args[], String titulo, String descripcion) throws Exception {
+		List<Petnoticia> listPetnoticia = null;
+		
+		Criteria criteria = session.createCriteria(Petnoticia.class)
+		.add( Restrictions.eq("setestado.idestado", 1));
+		
+		if(titulo != null && titulo.trim().length() > 0){
+            criteria.add( Restrictions.like("titulo", "%"+titulo.replaceAll(" ", "%")+"%").ignoreCase());
+        }
+        
+        if(descripcion != null && descripcion.trim().length() > 0){
+            criteria.add( Restrictions.like("descripcion", "%"+descripcion.replaceAll(" ", "%")+"%").ignoreCase());
+        }
+		
+        criteria.addOrder(Order.desc("fecharegistro"))
+		.setMaxResults(pageSize)
+		.setFirstResult(pageNumber);
+        
+		listPetnoticia = (List<Petnoticia>) criteria.list();
+		
+		if(listPetnoticia != null && listPetnoticia.size() > 0)
+		{
+			Criteria criteriaCount = session.createCriteria(Petnoticia.class)
+					.add( Restrictions.eq("setestado.idestado", 1))
+                    .setProjection( Projections.rowCount());
+
+			if(titulo != null && titulo.trim().length() > 0){
+	            criteria.add( Restrictions.like("titulo", "%"+titulo.replaceAll(" ", "%")+"%").ignoreCase());
+	        }
+	        
+	        if(descripcion != null && descripcion.trim().length() > 0){
+	            criteria.add( Restrictions.like("descripcion", "%"+descripcion.replaceAll(" ", "%")+"%").ignoreCase());
+	        }
+			
+			criteriaCount.setMaxResults(pageSize)
+			.setFirstResult(pageNumber);
+			
+			Object object = criteriaCount.uniqueResult();
+			int count = (object==null?0:Integer.parseInt(object.toString()));
+			args[0] = count;
+		}
+		else
+		{
+			args[0] = 0;
+		}
+		
+		return listPetnoticia;
+	}
+	
+	public void savePetnoticia(Session session, Petnoticia petnoticia) throws Exception {
+		session.save(petnoticia);
+	}
+	
+	public void updatePetnoticia(Session session, Petnoticia petnoticia) throws Exception {
+		session.update(petnoticia);
+	}
+	
+}
