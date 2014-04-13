97afb555dc3bca47025b1487c8491d23990bf45e
metalkarlos <Metalkarlos0923>
2014-04-12 19:58:34 -0500

    Avance

--- campofelizweb/src/com/web/cementerio/bean/NoticiaAdminBean.java
+++ campofelizweb/src/com/web/cementerio/bean/NoticiaAdminBean.java
@@ -1,11 +1,17 @@
 package com.web.cementerio.bean;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
+import com.web.cementerio.bo.PetnoticiaBO;
+import com.web.cementerio.pojo.annotations.Petfotonoticia;
 import com.web.cementerio.pojo.annotations.Petnoticia;
+import com.web.util.FileUtil;
+import com.web.util.MessageUtil;
 
 @ManagedBean
 @ViewScoped
@@ -17,9 +23,25 @@ public class NoticiaAdminBean implements Serializable {
 	private static final long serialVersionUID = -4748535305651371565L;
 	private int idnoticia;
 	private Petnoticia petnoticia;
+	private String rutaImagenes;
+	private List<Petfotonoticia> lisPetfotonoticia;
+	private Petfotonoticia petfotonoticiaSeleccionada;
 
 	public NoticiaAdminBean() {
 		petnoticia = new Petnoticia();
+		lisPetfotonoticia = new ArrayList<Petfotonoticia>();
+		petfotonoticiaSeleccionada = new Petfotonoticia();
+		
+		cargarRutaImagenes();
+	}
+	
+	private void cargarRutaImagenes(){
+		try {
+			rutaImagenes = new FileUtil().getPropertyValue("rutaImagen");
+		} catch (Exception e) {
+			e.printStackTrace();
+			new MessageUtil().showFatalMessage("Error!", "Ha ocurrido un error inesperado. Comunicar al Webmaster!");
+		}
 	}
 
 	public int getIdnoticia() {
@@ -28,6 +50,20 @@ public class NoticiaAdminBean implements Serializable {
 
 	public void setIdnoticia(int idnoticia) {
 		this.idnoticia = idnoticia;
+		
+		if(this.idnoticia > 0){
+			try {
+				PetnoticiaBO petnoticiaBO = new PetnoticiaBO();
+				petnoticia = petnoticiaBO.getPetnoticiaConObjetosById(idnoticia);
+				
+				if(petnoticia.getPetfotonoticias() != null && petnoticia.getPetfotonoticias().size() > 0){
+					lisPetfotonoticia = new ArrayList<Petfotonoticia>(petnoticia.getPetfotonoticias());
+				}
+			} catch(Exception e) {
+				e.printStackTrace();
+				new MessageUtil().showFatalMessage("Error!", "Ha ocurrido un error inesperado. Comunicar al Webmaster!");
+			}
+		}
 	}
 
 	public Petnoticia getPetnoticia() {
@@ -36,6 +72,31 @@ public class NoticiaAdminBean implements Serializable {
 
 	public void setPetnoticia(Petnoticia petnoticia) {
 		this.petnoticia = petnoticia;
+	}
+
+	public String getRutaImagenes() {
+		return rutaImagenes;
+	}
+
+	public void setRutaImagenes(String rutaImagenes) {
+		this.rutaImagenes = rutaImagenes;
+	}
+
+	public List<Petfotonoticia> getLisPetfotonoticia() {
+		return lisPetfotonoticia;
+	}
+
+	public void setLisPetfotonoticia(List<Petfotonoticia> lisPetfotonoticia) {
+		this.lisPetfotonoticia = lisPetfotonoticia;
+	}
+
+	public Petfotonoticia getPetfotonoticiaSeleccionada() {
+		return petfotonoticiaSeleccionada;
+	}
+
+	public void setPetfotonoticiaSeleccionada(
+			Petfotonoticia petfotonoticiaSeleccionada) {
+		this.petfotonoticiaSeleccionada = petfotonoticiaSeleccionada;
 	}
 
 }
