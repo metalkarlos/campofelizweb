a2068775759b9e6e04ac54b41c492fb79612f752
metalkarlos <Metalkarlos0923>
2014-04-15 00:51:04 -0500

    equal & hashcode

--- campofelizweb/src/com/web/cementerio/pojo/annotations/Petfotonoticia.java
+++ campofelizweb/src/com/web/cementerio/pojo/annotations/Petfotonoticia.java
@@ -156,4 +156,88 @@ public class Petfotonoticia implements java.io.Serializable {
 		this.iplog = iplog;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((descripcion == null) ? 0 : descripcion.hashCode());
+		result = prime * result
+				+ ((fecharegistro == null) ? 0 : fecharegistro.hashCode());
+		result = prime * result + idfotonoticia;
+		result = prime * result + ((iplog == null) ? 0 : iplog.hashCode());
+		result = prime * result
+				+ ((nombrearchivo == null) ? 0 : nombrearchivo.hashCode());
+		result = prime * result + ((perfil == null) ? 0 : perfil.hashCode());
+		result = prime * result
+				+ ((petnoticia == null) ? 0 : petnoticia.getIdnoticia());
+		result = prime * result + ((ruta == null) ? 0 : ruta.hashCode());
+		result = prime * result
+				+ ((setestado == null) ? 0 : setestado.getIdestado());
+		result = prime * result
+				+ ((setusuario == null) ? 0 : setusuario.getIdusuario());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Petfotonoticia other = (Petfotonoticia) obj;
+		if (descripcion == null) {
+			if (other.descripcion != null)
+				return false;
+		} else if (!descripcion.equals(other.descripcion))
+			return false;
+		if (fecharegistro == null) {
+			if (other.fecharegistro != null)
+				return false;
+		} else if (!fecharegistro.equals(other.fecharegistro))
+			return false;
+		if (idfotonoticia != other.idfotonoticia)
+			return false;
+		if (iplog == null) {
+			if (other.iplog != null)
+				return false;
+		} else if (!iplog.equals(other.iplog))
+			return false;
+		if (nombrearchivo == null) {
+			if (other.nombrearchivo != null)
+				return false;
+		} else if (!nombrearchivo.equals(other.nombrearchivo))
+			return false;
+		if (perfil == null) {
+			if (other.perfil != null)
+				return false;
+		} else if (!perfil.equals(other.perfil))
+			return false;
+		if (petnoticia == null) {
+			if (other.petnoticia != null)
+				return false;
+		} else if (petnoticia.getIdnoticia() != other.petnoticia.getIdnoticia())
+			return false;
+		if (ruta == null) {
+			if (other.ruta != null)
+				return false;
+		} else if (!ruta.equals(other.ruta))
+			return false;
+		if (setestado == null) {
+			if (other.setestado != null)
+				return false;
+		} else if (setestado.getIdestado() != other.setestado.getIdestado())
+			return false;
+		if (setusuario == null) {
+			if (other.setusuario != null)
+				return false;
+		} else if (setusuario.getIdusuario() != other.setusuario.getIdusuario())
+			return false;
+		return true;
+	}
+	
+	
+
 }
