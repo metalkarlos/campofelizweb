00539371b3307e70cdfa1acbbec30f603b3e4354
metalkarlos <Metalkarlos0923>
2014-04-16 12:54:44 -0500

    avance

--- campofelizweb/src/com/web/cementerio/bean/NoticiaAdminBean.java
+++ campofelizweb/src/com/web/cementerio/bean/NoticiaAdminBean.java
@@ -11,6 +11,7 @@ import javax.faces.bean.ViewScoped;
 import org.primefaces.event.FileUploadEvent;
 import org.primefaces.model.DefaultStreamedContent;
 import org.primefaces.model.StreamedContent;
+import org.primefaces.model.UploadedFile;
 
 import com.web.cementerio.bo.PetnoticiaBO;
 import com.web.cementerio.pojo.annotations.Petfotonoticia;
@@ -29,16 +30,23 @@ public class NoticiaAdminBean implements Serializable {
 	private static final long serialVersionUID = -4748535305651371565L;
 	private int idnoticia;
 	private Petnoticia petnoticia;
+	private Petnoticia petnoticiaClon;
 	private String rutaImagenes;
 	private List<Petfotonoticia> lisPetfotonoticia;
+	private List<Petfotonoticia> lisPetfotonoticiaClon;
 	private Petfotonoticia petfotonoticiaSeleccionada;
 	private StreamedContent streamedContent;
+	private UploadedFile uploadedFile;
+	private String descripcionFoto;
 	private boolean fotoSubida;
 	
 	public NoticiaAdminBean() {
 		petnoticia = new Petnoticia();
+		petnoticiaClon = new Petnoticia();
 		lisPetfotonoticia = new ArrayList<Petfotonoticia>();
+		lisPetfotonoticiaClon = new ArrayList<Petfotonoticia>();
 		petfotonoticiaSeleccionada = new Petfotonoticia();
+		descripcionFoto = "";
 		fotoSubida = false;
 		
 		cargarRutaImagenes();
@@ -69,8 +77,16 @@ public class NoticiaAdminBean implements Serializable {
 				PetnoticiaBO petnoticiaBO = new PetnoticiaBO();
 				petnoticia = petnoticiaBO.getPetnoticiaConObjetosById(idnoticia);
 				
-				if(petnoticia.getPetfotonoticias() != null && petnoticia.getPetfotonoticias().size() > 0){
-					lisPetfotonoticia = new ArrayList<Petfotonoticia>(petnoticia.getPetfotonoticias());
+				if(petnoticia != null && petnoticia.getIdnoticia() > 0){
+					petnoticiaClon = petnoticia.clonar();
+					
+					if(petnoticia.getPetfotonoticias() != null && petnoticia.getPetfotonoticias().size() > 0){
+						lisPetfotonoticia = new ArrayList<Petfotonoticia>(petnoticia.getPetfotonoticias());
+						
+						for(Petfotonoticia petfotonoticia : lisPetfotonoticia){
+							lisPetfotonoticiaClon.add(petfotonoticia.clonar());
+						}
+					}
 				}
 			} catch(Exception e) {
 				e.printStackTrace();
@@ -81,6 +97,7 @@ public class NoticiaAdminBean implements Serializable {
 
 	public void handleFileUpload(FileUploadEvent event) {
 		try{
+			uploadedFile = event.getFile();
 			streamedContent = new DefaultStreamedContent(event.getFile().getInputstream(), event.getFile().getContentType());
 			
 			FacesUtil facesUtil = new FacesUtil();
@@ -97,21 +114,40 @@ public class NoticiaAdminBean implements Serializable {
 	}
 	
 	public void ponerFotoPrincipal(){
-		new MessageUtil().showFatalMessage("pri!", "Ha ocurrido un error inesperado. Comunicar al Webmaster!");
+		petnoticia.setRutafoto(petfotonoticiaSeleccionada.getRuta());
+		petfotonoticiaSeleccionada = new Petfotonoticia();
+		new MessageUtil().showInfoMessage("Listo!", "Se ha seleccionado como foto principal");
 	}
 	
 	public void quitarFotoGaleria(){
 		lisPetfotonoticia.remove(petfotonoticiaSeleccionada);
-		new MessageUtil().showFatalMessage("qui!", "Ha ocurrido un error inesperado. Comunicar al Webmaster!");
+		petfotonoticiaSeleccionada = new Petfotonoticia();
 	}
 	
 	public void borrarFotoSubida(){
 		streamedContent = null;
+		uploadedFile = null;
 		fotoSubida = false;
 	}
 	
 	public void grabar(){
-		new MessageUtil().showFatalMessage("grab!", "Ha ocurrido un error inesperado. Comunicar al Webmaster!");
+		try{
+			PetnoticiaBO petnoticiaBO = new PetnoticiaBO();
+			Petfotonoticia petfotonoticia = new Petfotonoticia();
+			petfotonoticia.setDescripcion(descripcionFoto);
+			
+			if(idnoticia == 0){
+				
+				
+				petnoticiaBO.ingresar(petnoticia, petfotonoticia, uploadedFile);
+			}else{
+				petnoticiaBO.modificar(petnoticia, petnoticiaClon, lisPetfotonoticia, lisPetfotonoticiaClon, petfotonoticia, uploadedFile);
+			}
+			new MessageUtil().showInfoMessage("Listo!", "Datos grabados con Exito!");
+		}catch(Exception e){
+			e.printStackTrace();
+			new MessageUtil().showFatalMessage("Error!", "Ha ocurrido un error inesperado. Comunicar al Webmaster!");
+		}
 	}
 
 	public int getIdnoticia() {
@@ -169,5 +205,13 @@ public class NoticiaAdminBean implements Serializable {
 
 	public void setFotoSubida(boolean fotoSubida) {
 		this.fotoSubida = fotoSubida;
+	}
+
+	public String getDescripcionFoto() {
+		return descripcionFoto;
+	}
+
+	public void setDescripcionFoto(String descripcionFoto) {
+		this.descripcionFoto = descripcionFoto;
 	}
 }
--- campofelizweb/src/com/web/cementerio/bo/PetnoticiaBO.java
+++ campofelizweb/src/com/web/cementerio/bo/PetnoticiaBO.java
@@ -1,14 +1,20 @@
 package com.web.cementerio.bo;
 
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
 import org.hibernate.Session;
+import org.primefaces.model.UploadedFile;
 
 import com.web.cementerio.bean.UsuarioBean;
+import com.web.cementerio.dao.PetfotonoticiaDAO;
 import com.web.cementerio.dao.PetnoticiaDAO;
+import com.web.cementerio.pojo.annotations.Petfotonoticia;
 import com.web.cementerio.pojo.annotations.Petnoticia;
+import com.web.cementerio.pojo.annotations.Setestado;
 import com.web.util.FacesUtil;
+import com.web.util.FileUtil;
 import com.web.util.HibernateUtil;
 
 public class PetnoticiaBO {
@@ -73,7 +79,7 @@ public class PetnoticiaBO {
 		return listPetnoticia;
 	}
 	
-	public boolean grabar(Petnoticia petnoticia) throws Exception {
+	public boolean ingresar(Petnoticia petnoticia, Petfotonoticia petfotonoticia, UploadedFile uploadedFile) throws Exception {
 		boolean ok = false;
 		Session session = null;
 		
@@ -81,17 +87,59 @@ public class PetnoticiaBO {
 			session = HibernateUtil.getSessionFactory().openSession();
 			session.beginTransaction();
 			
-			int maxid = petnoticiaDAO.maxIdnoticia(session)+1;
-			Date fecharegistro = new Date();
 			UsuarioBean usuarioBean = (UsuarioBean)new FacesUtil().getSessionBean("usuarioBean");
+			PetfotonoticiaDAO petfotonoticiaDAO = new PetfotonoticiaDAO();
 			
-			petnoticia.setIdnoticia(maxid);
+			//noticia
+			int maxIdnoticia = petnoticiaDAO.maxIdnoticia(session)+1;
+			petnoticia.setIdnoticia(maxIdnoticia);
+			Setestado setestadoPetnoticia = new Setestado();
+			setestadoPetnoticia.setIdestado(1);
+			petnoticia.setSetestado(setestadoPetnoticia);
+
+			//auditoria
+			Date fecharegistro = new Date();
 			petnoticia.setFecharegistro(fecharegistro);
 			petnoticia.setIplog(usuarioBean.getIp());
-			petnoticia.setSetestado(usuarioBean.getSetUsuario().getSetestado());
 			petnoticia.setSetusuario(usuarioBean.getSetUsuario());
 	
+			//ingresar noticia
 			petnoticiaDAO.savePetnoticia(session, petnoticia);
+			
+			//foto en disco
+			FileUtil fileUtil = new FileUtil();
+			Calendar fecha = Calendar.getInstance();
+			
+			String rutaImagenes = fileUtil.getPropertyValue("rutaImagen");
+			String rutaNoticias =  "/noticia/" + fecha.get(Calendar.YEAR);
+			String nombreArchivo = fecha.get(Calendar.MONTH) + "-" + fecha.get(Calendar.DAY_OF_MONTH) + "-" + fecha.get(Calendar.YEAR) + "-" + maxIdnoticia + fileUtil.getFileExtention(uploadedFile.getFileName());
+			
+			String rutaCompleta = rutaImagenes + rutaNoticias;
+			
+			if(fileUtil.createDir(rutaCompleta)){
+				//crear foto en disco
+				fileUtil.createFile(rutaNoticias+"/"+nombreArchivo,uploadedFile.getContents());
+			}
+			
+			//foto en BD
+			int maxIdfotonoticia = petfotonoticiaDAO.maxIdfotonoticia(session);
+			petfotonoticia.setIdfotonoticia(maxIdfotonoticia);
+			petfotonoticia.setPetnoticia(petnoticia);
+			petfotonoticia.setRuta(rutaNoticias+"/"+nombreArchivo);
+			petfotonoticia.setNombrearchivo(nombreArchivo);
+			petfotonoticia.setPerfil(1);//campo sin uso ya que tabla principal posee ruta de foto de perfil
+			Setestado setestadoPetfotonoticia = new Setestado();
+			setestadoPetfotonoticia.setIdestado(1);
+			petfotonoticia.setSetestado(setestadoPetfotonoticia);
+			
+			//auditoria
+			fecharegistro = new Date();
+			petfotonoticia.setFecharegistro(fecharegistro);
+			petfotonoticia.setIplog(usuarioBean.getIp());
+			petfotonoticia.setSetusuario(usuarioBean.getSetUsuario());
+			
+			//ingresar foto en BD
+			petfotonoticiaDAO.savePetfotonoticia(session, petfotonoticia);
 			
 			session.getTransaction().commit();
 			
@@ -104,5 +152,101 @@ public class PetnoticiaBO {
 		}
 		
 		return ok;
+	}
+	
+	//TODO completar, fotos quitadas eliminar del disco
+	public boolean modificar(Petnoticia petnoticia, Petnoticia petnoticiaClon, List<Petfotonoticia> lisPetfotonoticia, List<Petfotonoticia> lisPetfotonoticiaClon, Petfotonoticia petfotonoticia, UploadedFile uploadedFile) throws Exception {
+		boolean ok = false;
+		Session session = null;
+		
+		try{
+			session = HibernateUtil.getSessionFactory().openSession();
+			session.beginTransaction();
+			
+			UsuarioBean usuarioBean = (UsuarioBean)new FacesUtil().getSessionBean("usuarioBean");
+			PetfotonoticiaDAO petfotonoticiaDAO = new PetfotonoticiaDAO();
+			Date fecharegistro = new Date();
+			
+			//Se graba la noticia si han habido cambios
+			if(!petnoticia.equals(petnoticiaClon)){
+				//auditoria
+				petnoticiaClon.setFecharegistro(fecharegistro);
+				petnoticiaClon.setIplog(usuarioBean.getIp());
+				petnoticiaClon.setSetusuario(usuarioBean.getSetUsuario());
+		
+				//actualizar
+				petnoticiaDAO.updatePetnoticia(session, petnoticiaClon);
+			}
+			
+			//Si han quitado fotos se las inhabilita en la BD
+			for(Petfotonoticia petfotonoticiaItem : lisPetfotonoticia){
+				if(!lisPetfotonoticiaClon.contains(petfotonoticiaItem)){
+					//inhabilitar
+					petfotonoticiaItem.getSetestado().setIdestado(0);
+					
+					//auditoria
+					fecharegistro = new Date();
+					petfotonoticiaItem.setFecharegistro(fecharegistro);
+					petfotonoticiaItem.setIplog(usuarioBean.getIp());
+					petfotonoticiaItem.setSetusuario(usuarioBean.getSetUsuario());
+					
+					//actualizar
+					petfotonoticiaDAO.updatePetfotonoticia(session, petfotonoticiaItem);
+				}
+			}
+			
+			//
+			
+			session.getTransaction().commit();
+			
+			ok = true;
+		}catch(Exception e){
+			session.getTransaction().rollback();
+			throw new Exception(e); 
+		}finally{
+			session.close();
+		}
+		
+		return ok;
+	}
+	
+	private void creaFotoDiscoBD(String nombreFoto, Petnoticia petnoticia, Petfotonoticia petfotonoticia, UploadedFile uploadedFile, Session session) throws Exception {
+		UsuarioBean usuarioBean = (UsuarioBean)new FacesUtil().getSessionBean("usuarioBean");
+		PetfotonoticiaDAO petfotonoticiaDAO = new PetfotonoticiaDAO();
+		
+		//foto en disco
+		FileUtil fileUtil = new FileUtil();
+		Calendar fecha = Calendar.getInstance();
+		
+		String rutaImagenes = fileUtil.getPropertyValue("rutaImagen");
+		String rutaNoticias =  "/noticia/" + fecha.get(Calendar.YEAR);
+		String nombreArchivo = fecha.get(Calendar.MONTH) + "-" + fecha.get(Calendar.DAY_OF_MONTH) + "-" + fecha.get(Calendar.YEAR) + "-" + nombreFoto + fileUtil.getFileExtention(uploadedFile.getFileName());
+		
+		String rutaCompleta = rutaImagenes + rutaNoticias;
+		
+		if(fileUtil.createDir(rutaCompleta)){
+			//crear foto en disco
+			fileUtil.createFile(rutaNoticias+"/"+nombreArchivo,uploadedFile.getContents());
+		}
+		
+		//foto en BD
+		int maxIdfotonoticia = petfotonoticiaDAO.maxIdfotonoticia(session);
+		petfotonoticia.setIdfotonoticia(maxIdfotonoticia);
+		petfotonoticia.setPetnoticia(petnoticia);
+		petfotonoticia.setRuta(rutaNoticias+"/"+nombreArchivo);
+		petfotonoticia.setNombrearchivo(nombreArchivo);
+		petfotonoticia.setPerfil(1);//campo sin uso ya que tabla principal posee ruta de foto de perfil
+		Setestado setestadoPetfotonoticia = new Setestado();
+		setestadoPetfotonoticia.setIdestado(1);
+		petfotonoticia.setSetestado(setestadoPetfotonoticia);
+		
+		//auditoria
+		Date fecharegistro = new Date();
+		petfotonoticia.setFecharegistro(fecharegistro);
+		petfotonoticia.setIplog(usuarioBean.getIp());
+		petfotonoticia.setSetusuario(usuarioBean.getSetUsuario());
+		
+		//ingresar foto en BD
+		petfotonoticiaDAO.savePetfotonoticia(session, petfotonoticia);
 	}
 }
