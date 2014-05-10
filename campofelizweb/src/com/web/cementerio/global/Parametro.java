6e1fc0056e47186484f8788c546f63a713002009
metalkarlos <Metalkarlos0923>
2014-05-09 19:47:14 -0500

    se crea parametro PROPERTIES_MAIL con el nombre del archivo de propiedades de los parametros de correo

--- campofelizweb/src/com/web/cementerio/global/Parametro.java
+++ campofelizweb/src/com/web/cementerio/global/Parametro.java
@@ -8,21 +8,8 @@ import javax.faces.bean.SessionScoped;
 public class Parametro {
 	public static final String FILE_SEPARATOR = "/";//File.separator;
 	public static final long DAY_IN_MILLISECONDS = (24*60*60*1000);
-	/*public static final String WAR_PATH;
-	static{
-		String war_path = null;
-		try{
-			war_path = new FacesUtil().getRealPath("");
-		}
-		catch(Exception e){
-			e.printStackTrace();
-		}
-		WAR_PATH = war_path;
-	}*/
-	//public static final String DEPLOYMENTS_PATH = WAR_PATH.substring(0, WAR_PATH.lastIndexOf("\\"));
-	//public static final String PARAMETROS_PROPERTIES_PATH = WAR_PATH+FILE_SEPARATOR+"resources"+FILE_SEPARATOR+"parametros.properties";
-	//public static final String RUTA_REPORTES = WAR_PATH+FILE_SEPARATOR+"reportes"+FILE_SEPARATOR;
 	public static final String PROPERTIES_FILE_NAME = "parametros.properties";
+	public static final String PROPERTIES_MAIL = "mail.properties";
 	
 	//Seccion de rangos de los cargos
 	public static final short CARGO_NIVEL_SERVICIO_MIN = 1;
