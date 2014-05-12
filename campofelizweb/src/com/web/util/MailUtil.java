030fea58dcf82eeffc6a57bc8d9aefef62d76aa1
metalkarlos <Metalkarlos0923>
2014-05-11 23:48:30 -0500

    cambios

--- campofelizweb/src/com/web/util/MailUtil.java
+++ campofelizweb/src/com/web/util/MailUtil.java
@@ -14,52 +14,73 @@ import com.web.cementerio.global.Parametro;
 public class MailUtil {
 
 	private String host;
-	private String to;
+	private String usuario;
 	private String clave;
+	private String port;
 	
-	public void enviarMail(String from, String asunto, String mensaje) throws Exception {
+	public MailUtil() {
+		host = "";
+		usuario = "";
+		clave = "";
+		port = "";
+	}
+	
+	/***
+	 * Envía correo
+	 * @param destinatario Si es null se envía únicamente al administrador
+	 * @param asunto
+	 * @param contenido
+	 * @throws Exception
+	 */
+	public void enviarMail(String destinatario, String asunto, String contenido) throws Exception {
 		Properties propertiesMail = new FileUtil()
 				.getPropertiesFile(Parametro.PROPERTIES_MAIL);
 		host = propertiesMail.getProperty("host");
-		to = propertiesMail.getProperty("usuario");
+		usuario = propertiesMail.getProperty("usuario");
 		clave = propertiesMail.getProperty("clave");
+		port = propertiesMail.getProperty("port");
 
-		Properties properties = System.getProperties();
+		//Properties properties = System.getProperties();
+		Properties properties = new Properties();
 		properties.put("mail.smtp.host", host);
-		properties.put("mail.smtp.port", "465");   
+		properties.put("mail.smtp.port", port);   
 		
-		//properties.put("mail.user", to);
-		//properties.put("mail.password", clave);
+		properties.put("mail.user", usuario);//
+		properties.put("mail.password", clave);//
 
 		properties.put("mail.smtp.starttls.enable","true");
 		//properties.put("mail.smtp.EnableSSL.enable","true");
 		properties.put("mail.smtp.auth", "true");
-		properties.put("mail.smtp.socketFactory.port", "465");
-		
+		properties.put("mail.smtp.socketFactory.port", port);
 		properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");   
 		properties.put("mail.smtp.socketFactory.fallback", "false");   
 		
 		properties.put("mail.debug", "true"); 
 		 
-		//Session mailSession = Session.getDefaultInstance(properties);
-		//Authenticator myAuth = new MyAuthenticator();
+		//Session mailSession = Session.getDefaultInstance(properties, null);
 		Session mailSession = Session.getDefaultInstance(properties, 
 			    new javax.mail.Authenticator(){
 			        protected PasswordAuthentication getPasswordAuthentication() {
 			            return new PasswordAuthentication(
-			            		to, clave);// Specify the Username and the PassWord
+			            		usuario, clave);
 			        }
 			});
 
 		mailSession.setDebug(true);
 		
 		MimeMessage message = new MimeMessage(mailSession);
-		message.setFrom(new InternetAddress(from));
-		message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
-		message.setSubject(asunto);
-		message.setContent(mensaje, "text/html");
+		message.setFrom(new InternetAddress(usuario));
+		destinatario = destinatario == null ? usuario : destinatario;
+		message.addRecipient(Message.RecipientType.TO, new InternetAddress(destinatario));
+		message.setSubject(asunto, "utf-8");
+		message.setContent(contenido, "text/html");
 		
 		Transport.send(message);
+		//Transport tr = mailSession.getTransport("smtp");
+		//tr.connect(host, to, clave);
+		//message.saveChanges();      // don't forget this
+		//tr.sendMessage(message, message.getAllRecipients());
+		//tr.close();
 		
 		mailSession.getDebugOut();
 	}
