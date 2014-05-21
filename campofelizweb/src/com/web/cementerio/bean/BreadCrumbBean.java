4935dc3ae39497198b54f4953546a8af05748bea
metalkarlos <metalkarlos@gmail.com>
2014-05-21 12:10:27 -0500

    Se crea bean de sesion para manejo del breadcrumb

---
+++ campofelizweb/src/com/web/cementerio/bean/BreadCrumbBean.java
@@ -0,0 +1,69 @@
+package com.web.cementerio.bean;
+
+import java.util.Arrays;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import org.primefaces.component.menuitem.MenuItem;
+import org.primefaces.model.DefaultMenuModel;
+import org.primefaces.model.MenuModel;
+
+@ManagedBean
+@SessionScoped
+public class BreadCrumbBean {
+
+	private MenuModel breadCrumb;
+	private List<String> opciones = Arrays.asList("quienessomos.jsf",
+			"servicios.jsf",
+			"servicio.jsf",
+			"encuesta.jsf",
+			"cotizacion.jsf",
+			"cementeriovirtual.jsf",
+			"mascotashomenaje.jsf",
+			"noticias.jsf",
+			"noticia.jsf",
+			"guiageneral.jsf",
+			"preguntas.jsf",
+			"contactenos.jsf");
+	
+	private String[][] ruta = {{"Quienes Somos","home.jsf;#"},
+			{"Home;Servicios","home.jsf;#"},
+			{"Home;Servicios;Servicio","home.jsf;servicios.jsf;#"},
+			{"Home;Encuesta","home.jsf;#"},
+			{"Home;Cotización","home.jsf;#"},
+			{"Home;Cementerio Virtual","home.jsf;#"},
+			{"Home;Mascota Homenaje","home.jsf;#"},
+			{"Home;Noticias","home.jsf;#"},
+			{"Home;Noticias;Noticia","home.jsf;noticias.jsf;#"},
+			{"Home;Guía General","home.jsf;#"},
+			{"Home;Preguntas Frecuentes","home.jsf;#"},
+			{"Home;Contáctenos","home.jsf;contactenos.jsf"}};
+	
+	public BreadCrumbBean() {
+		breadCrumb = new DefaultMenuModel();
+	}
+	
+	public void armarBreadCrumb(String opcion){
+		int index = opciones.indexOf(opcion);
+		breadCrumb = new DefaultMenuModel();
+		
+		if(index >= 0){
+			String txtnom = ruta[index][0];
+			String[] nom = txtnom.split(";");
+			
+			String txtop = ruta[index][1];
+			String[] op = txtop.split(";");
+
+			for(int i = 0 ; i<op.length ; i++){
+				MenuItem item = new MenuItem();
+				item.setValue(nom[i]);
+				item.setUrl(op[i]);
+				breadCrumb.addMenuItem(item);
+			}
+		}
+	}
+	
+	public MenuModel getBreadCrumb() { return breadCrumb; }
+}
